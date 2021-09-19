0d8ee3075286d951d76942b64b1b687baa2723ee
Maxim Zuyzko <maximzuyzko@gmail.com>
2021-09-20 02:12:37 +0400

    feat: restyle cv, add project links

---
+++ index.js
@@ -0,0 +1,20 @@
+const projects = document.querySelectorAll('.projects__item');
+
+function addOpen() {
+    this.classList.add('open');
+}
+
+function removeOpen() {
+    this.classList.remove('open');
+}
+
+function toggleActive(e) {
+    console.log(e.propertyName)
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+}
+
+projects.forEach(project => project.addEventListener('mouseenter', addOpen));
+projects.forEach(project => project.addEventListener('mouseleave', removeOpen));
+projects.forEach(project => project.addEventListener('transitionend', toggleActive));
